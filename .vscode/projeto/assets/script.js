d2372c8f317b2804a069a2f1f4293ed4dc6443e9
notHackk <luccassr98@gmail.com>
2024-04-05 22:22:00 -0300

    add final img

--- .vscode/projeto/assets/script.js
+++ .vscode/projeto/assets/script.js
@@ -9,17 +9,17 @@ function togglemode() {
   //}
   // A maracaçao a acima é caso voce queira criar a function do zero, porem ja existe a mesma funçao ja criada no javascript: chamada (TOGGLE) que nos criamos no html, na tag do (#switch) e usaremos essa function abaixo//
 
-// Para fazer a troca da imagem do avatar(img) pelo javascript.
+  // Para fazer a troca da imagem do avatar(img) pelo javascript.
 
-const img = document.querySelector("#profile img")
-// usaremos a sintaxe abaixo.
-// se estiver o modo light. adicinar a imagem light
-//
-// no exemplo acima eu selecionei onde a imagem esta localizada no documento css
-if (html.classList.contains("light")) {
-  img.setAttribute("src", "./avatar-light.png")
-} else {
-  img.setAttribute("src", "./tinywow_profile_photo_51512929.png")
-}
-// sempre verificar se as chaves {} estao corretas
+  const img = document.querySelector("#profile img")
+  // usaremos a sintaxe abaixo.
+  // se estiver o modo light. adicinar a imagem light
+  //
+  // no exemplo acima eu selecionei onde a imagem esta localizada no documento css
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./Manuel gome.jpeg")
+  } else {
+    img.setAttribute("src", "./tinywow_profile_photo_51512929.png")
+  }
+  // sempre verificar se as chaves {} estao corretas
 }
